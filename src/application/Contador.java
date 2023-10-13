ff44c18d53f509c3eb9386513bbabe90e0bd712c
Roger <rogerbritosan@gmail.com>
2023-10-13 18:36:51 -0300

    Desafio DIO - Gleyson Sampaio - Controle de Fluxo em Java

---
+++ src/application/Contador.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Scanner;
+
+import model.exceptions.ParametrosInvalidosException;
+
+public class Contador {
+
+	public static void main(String[] args) {
+
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+
+		System.out.println();
+
+		try {
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException e) {
+			System.out.println(e.getMessage());
+		}
+
+		terminal.close();
+
+	}
+
+	public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+		}
+
+		int contagem = parametroDois - parametroUm;
+
+		for (int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo o número: " + i);
+		}
+
+	}
+
+}
---
+++ src/model/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+	
+	public ParametrosInvalidosException(String msg) {
+		super(msg);
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
